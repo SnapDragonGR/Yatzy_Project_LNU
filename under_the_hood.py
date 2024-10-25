75b2df93d7cff7fa0d6885ad09758087cbe8d897
SnapDragonGR <thesanegleb@gmail.com>
2024-10-25 15:28:01 +0200

    Initialized the score card dictionary for all combinations

---
+++ under_the_hood.py
@@ -0,0 +1,8 @@
+score_card = {
+    'ones': 0, 'twos': 0, 'threes': 0, 'fours': 0, 'fives': 0, 'sixes': 0,
+    'one_pair': 0, 'two_pairs': 0, 'three_of_a_kind': 0, 'four_of_a_kind': 0, 'small_straight': 0,
+    'large_straight': 0, 'full_house': 0, 'chance': 0, 'yatzy': 0, 'bonus': 0
+}
+
+
+
