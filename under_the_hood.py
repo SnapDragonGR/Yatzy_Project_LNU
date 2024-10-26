79040dc12bc2c5620f488a535be7889c0f4208b2
SnapDragonGR <thesanegleb@gmail.com>
2024-10-26 11:17:57 +0200

    Refactored the score_card dict, started adding score updating mechanics, corrected the show_scoring_sheet(), fixed the logic in the main loop

--- under_the_hood.py
+++ under_the_hood.py
@@ -1,10 +1,11 @@
 import random
 
 score_card = {
-    'ones': 0, 'twos': 0, 'threes': 0, 'fours': 0, 'fives': 0, 'sixes': 0,
-    'one_pair': 0, 'two_pairs': 0, 'three_of_a_kind': 0, 'four_of_a_kind': 0, 'small_straight': 0,
-    'large_straight': 0, 'full_house': 0, 'chance': 0, 'yatzy': 0, 'bonus': 0
+    'Ones': '-', 'Twos': '-', 'Threes': '-', 'Fours': '-', 'Fives': '-', 'Sixes': '-',
+    'One Pair': '-', 'Two Pairs': '-', 'Three of a Kind': '-', 'Four of a Kind': '-', 'Small Straight': '-',
+    'Large Straight': '-', 'Full House': '-', 'Chance': '-', 'Yatzy': '-'
 }
+
 
 def rolling_dice(game_mode_num): #game_mode_num is either 5 or 6 depending on the game mode chosen, ie chaging the number of times the dice are rolled
     #rolling hte dice
@@ -46,8 +47,78 @@ def print_rolls(roll_list, game_mode_num):
             print("gbiyasmlkb")
 
 
+# Give user possible categories based on what they have in their roll (if elifing at its max)
+def possible_categories(dice):
+    for num in dice:
+        pass
+
+
+# Start of counting functions (gon count the scores and update them in the score card)
+
+# count single digit score if a player decides to go for this option (make em choose the num to go for). Also, gotta have a global variable "dice" to have access to
+# the values of a roll and to be able to work with it
+def single_digits(dice, num):
+    score = 0
+    for i in dice:
+        if i == num:
+            score += num
+
+    key_mapping = {
+        1: 'Ones',
+        2: 'Twos',
+        3: 'Threes',
+        4: 'Fours',
+        5: 'Fives',
+        6: 'Sixes'
+    }
+
+    if num in key_mapping:
+        key = key_mapping[num]
+        score_card[key] = score
+
+# check if a number appears three (or more) times and update the Three of a Kind value if that's the case
+def three_of_a_kind(dice):
+    score = 0
+    for num in dice:
+        if dice.count(num) >= 3:
+            score = num * 3
+            break
+
+    score_card['Three of a Kind'] = score
+
+
+# same as above but for 4 appearances
+def four_of_a_kind(dice):
+    score = 0
+    for num in dice:
+        if dice.count(num) >= 4:
+            score = num * 4
+            break
+
+    score_card['Four of a Kind'] = score
+
+
+def small_straight(dice):
+    pass
+
+def large_straight(dice):
+    pass
+
+
+def full_house(dice):
+    pass
+
+
+def chance(dice):
+    pass
+
+
+def yatzy(dice):
+    pass
+
+
 # sum of scores from the upper section categories + bonus check
-upper_keys = ['ones', 'twos', 'threes', 'fours', 'fives', 'sixes']
+upper_keys = ['Ones', 'Twos', 'Threes', 'Fours', 'Fives', 'Sixes']
 def upper_count():
     upper_score = sum(score_card[key] for key in score_card if key in upper_keys)
 
@@ -63,9 +134,12 @@ def lower_count():
 
 def show_scoring_sheet():
     print('Current score sheet:')
+    for key, value in score_card.items():
+        print(f'{key}: {value}')
 
 
 def main():
-    while all(value == 0 for value in score_card.values()):   # correct the conditions
-        rolling_dice()
+    while any(isinstance(value, str) for value in score_card.values()):
+        print('Rolling dice...')
+        rolling_dice(5)
 
